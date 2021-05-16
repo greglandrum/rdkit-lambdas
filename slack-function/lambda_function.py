836c2b5c30a86bd8b374b5fbce3bc41e8787c4be
greg landrum <greg.landrum@gmail.com>
2021-05-16 08:46:11 +0200

    rename

--- slack-function/lambda_function.py
+++ slack-function/lambda_function.py
@@ -1,5 +1,27 @@
-# Copyright (C) 2018 greg Landrum and T5 Informatics GmbH
-# All Rights Reserved
+#
+# Copyright (C) 2018-2021 Greg Landrum
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#
+#  Created by Greg Landrum (greg.landrum@t5informatics.com), March 2018
+#
 import json,os,requests
 from urllib import parse
 from rdkit import Chem
